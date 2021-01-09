261c143c1b32145c6ee3327727b153b3e9cd74ad
Zhylow <pk1blue@yahoo.com>
2021-01-08 22:40:12 -0500

    added first button and question list

--- script.js
+++ script.js
@@ -1,50 +1,86 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
+var main = document.querySelector("main");
 
-// THEN a timer starts and I am presented with a question
-// click handler is triggered on the start button
-// timer starts
-// question shows up on the page
-// show the multiple choice answers
-
-
-// WHEN I answer a question
-// THEN I am presented with another question
-// click handler is triggered on what the user clicked on
-// shows if the answer is correct or not correct
-// remove the last question
-// remove the last answers
-// after a short time,
-// remove incorrect/correct,
-// show the new question
+var startbtn = document.createElement("button");
+main.appendChild(startbtn);
+startbtn.textContent = "CLICK HERE TO START THE QUIZ!"
+startbtn.style.backgroundColor = "rgb(146 255 101)"
+startbtn.style.marginLeft = "260px"
+startbtn.style.fontSize = "30px"
+startbtn.style.borderRadius = "20px"
+startbtn.style.cursor = "pointer"
 
 
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// if the answer is incorrect,
-// decrement the timer's time
+var countDown = document.createElement("h1")
+main.appendChild(countDown)
+countDown.style.textAlign = "center"
+countDown.style.fontSize = "60px"
 
 
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// stop the timer
-// remove the question from display
-// remove incorrect/correct
-// show quiz results
 
-// WHEN the game is over
-// THEN I can save my initials and score
-// show a text box for putting my name in
-// take the current high score from storage
-// modify the high score to include the user
-// save the current high score into storage
-// remove input form
-// show the high score page
+function begin () {
+    startbtn.style.display = "none"
+    var clickCount = 5
+    var prepTimer = setInterval(function() {
+        countDown.textContent = "starting quiz in " + clickCount
+        if (clickCount === 0) {
+            clearInterval(prepTimer)
+            countDown.style.display = "none"
+        }
+        clickCount--
+    }, 1000)
+}
 
-// Some of the things that I need are...
-// Array of questions
-// Timer
-// Click handlers (functions that do click events)
-// HTML templates for Right/Wrong, Questions, Answers, High scores
-// Stylesheets for each template
-// Array of objects for the high scores (Scoreboard and a way to save it)
+startbtn.addEventListener("click", begin)
+
+var questionArr = [
+    {
+        question: "what does the prompt command do?",
+        answerIndex: 2,
+        choices: [
+            "logs information to the console",
+            "creates a variable",
+            "asks the user for input",
+            "creates a function"
+        ]
+    },
+    {
+        question: "What type of value can a variable hold?",
+        answerIndex: 3,
+        choices: [
+            "numbers",
+            "strings",
+            "objects",
+            "all the above"
+        ]
+    },
+    {
+        question: "How do you incriment a for loop by 1",
+        answerIndex: 0,
+        choices: [
+            "i++",
+            "1+1",
+            "i--",
+            "++i"
+        ]
+    },
+    {
+        question: "Which tag would you use to add a javascript file to your html",
+        answerIndex: 1,
+        choices: [
+            "<stlye>",
+            "<script>",
+            "<link>",
+            "<meta>"
+        ]
+    },
+    {
+        question: "Which of the following uses conditional statements exclusively",
+        answerIndex: 2,
+        choices: [
+            "for loops",
+            "functions",
+            "if statements",
+            "intervals"
+        ]
+    },
+];
