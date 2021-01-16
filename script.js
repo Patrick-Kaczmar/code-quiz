f1f013b030af130cff32b8485086b2d8849a183d
Zhylow <pk1blue@yahoo.com>
2021-01-15 20:50:52 -0500

    added line breaks

--- script.js
+++ script.js
@@ -2,38 +2,38 @@ var main = document.querySelector("main");
 
 var footer = document.querySelector("footer");
 
-var body = document.querySelector("body")
+var body = document.querySelector("body");
 
-var form = document.querySelector("#form")
+var form = document.querySelector("#form");
 
-var score = document.querySelector("#score")
+var score = document.querySelector("#score");
 
-var subBtn = document.querySelector("#subBtn")
+var subBtn = document.querySelector("#subBtn");
 
-var scoreMsg = document.querySelector("#scoreMsg")
+var scoreMsg = document.querySelector("#scoreMsg");
 
-var showScore = document.querySelector("#showScore")
+var showScore = document.querySelector("#showScore");
 
-var clearScore = document.querySelector("#clearScore")
+var clearScore = document.querySelector("#clearScore");
 
 var startbtn = document.createElement("button");
 main.appendChild(startbtn);
-startbtn.textContent = "CLICK HERE TO START THE QUIZ!"
-startbtn.style.backgroundColor = "rgb(146 255 101)"
-startbtn.style.marginLeft = "260px"
-startbtn.style.fontSize = "30px"
-startbtn.style.borderRadius = "20px"
-startbtn.style.cursor = "pointer"
+startbtn.textContent = "CLICK HERE TO START THE QUIZ!";
+startbtn.style.backgroundColor = "rgb(146 255 101)";
+startbtn.style.marginLeft = "260px";
+startbtn.style.fontSize = "30px";
+startbtn.style.borderRadius = "20px";
+startbtn.style.cursor = "pointer";
 
 
-var countDown = document.createElement("h1")
-main.appendChild(countDown)
-countDown.style.textAlign = "center"
-countDown.style.fontSize = "60px"
+var countDown = document.createElement("h1");
+main.appendChild(countDown);
+countDown.style.textAlign = "center";
+countDown.style.fontSize = "60px";
 
-var questionIndex = 0
+var questionIndex = 0;
 
-var timer = 60
+var timer = 60;
 
 var questionArr = [
     {
@@ -88,79 +88,79 @@ var questionArr = [
     },
 ];
 
-renderScore()
+renderScore();
 
 function begin () {
 
-    startbtn.style.display = "none"
+    startbtn.style.display = "none";
 
-    var clickCount = 5
+    var clickCount = 5;
 
     var prepTimer = setInterval(function() {
-        countDown.textContent = "starting quiz in " + clickCount
+        countDown.textContent = "starting quiz in " + clickCount;
         if (clickCount === 0) {
-            clearInterval(prepTimer)
-            countDown.textContent = ""
+            clearInterval(prepTimer);
+            countDown.textContent = "";
             showTimer()
-        }
+        };
         clickCount--
-        console.log("seconds until quiz starts " + clickCount)
-    }, 1000)
-}
+        console.log("seconds until quiz starts " + clickCount);
+    }, 1000);
+};
 
 function showTimer() {
     var startTimer = setInterval(function() {
-        countDown.textContent = timer
+        countDown.textContent = timer;
         if (timer <= 0 || questionIndex > questionArr.length - 1) {
-            clearInterval(startTimer)
-            main.textContent = ""
-        footer.textContent = ""
+            clearInterval(startTimer);
+            main.textContent = "";
+        footer.textContent = "";
             highScore()
         }
         timer--
     }, 1000)
-    nextQuestion()
+    nextQuestion();
 }
 
 function nextQuestion() {
 
     if (questionIndex > questionArr.length - 1) {
-        main.textContent = ""
-        footer.textContent = ""
+        main.textContent = "";
+        footer.textContent = "";
         highScore()
         return
     }
 
-    footer.textContent = ""
+    footer.textContent = "";
 
-    var h1 = document.createElement("h1")
-    footer.appendChild(h1)
+    var h1 = document.createElement("h1");
+    footer.appendChild(h1);
 
-    h1.textContent = questionArr[questionIndex].question
+    h1.textContent = questionArr[questionIndex].question;
     
-    var ol = document.createElement("ol")
-    footer.appendChild(ol)
+    var ol = document.createElement("ol");
+    footer.appendChild(ol);
 
 
     for (i = 0; i < questionArr[i].choices.length; i++) {
-        var li = document.createElement("li")
-        ol.appendChild(li)
-        var choice = document.createElement("button")
-        choice.setAttribute("data-index", i)
-        li.appendChild(choice)
-        choice.style.fontSize = "30px"
-        choice.style.borderRadius = "20px"
-        choice.style.margin = "15px"
-        choice.style.backgroundColor = "rgb(66 178 245)"
-        choice.style.cursor = "pointer"
-        choice.textContent = questionArr[questionIndex].choices[i]
+        var li = document.createElement("li");
+        ol.appendChild(li);
+        var choice = document.createElement("button");
+        choice.setAttribute("data-index", i);
+        li.appendChild(choice);
+        choice.style.fontSize = "30px";
+        choice.style.borderRadius = "20px";
+        choice.style.margin = "15px";
+        choice.style.backgroundColor = "rgb(66 178 245)";
+        choice.style.cursor = "pointer";
+        choice.textContent = questionArr[questionIndex].choices[i];
 
 
         choice.addEventListener("click", function(event) {
-            var element = event.target
-            console.log(element)
-            var btnIndex = element.getAttribute("data-index")
-            console.log(btnIndex)
+            var element = event.target;
+            console.log(element);
+            var btnIndex = element.getAttribute("data-index");
+            console.log(btnIndex);
             if (parseInt(btnIndex) !== questionArr[questionIndex].answerIndex) {
                 timer += -10
                 questionIndex++
@@ -169,7 +169,7 @@ function nextQuestion() {
                 questionIndex++
                 nextQuestion()
             }
-        })
+        });
     }
 }
 
@@ -177,24 +177,24 @@ function highScore () {
     if (timer < 0) {
         timer = 0
     }
-    form.style.display = "block"
-    scoreMsg.textContent = "Submit your initials to save your score of " + timer
+    form.style.display = "block";
+    scoreMsg.textContent = "Submit your initials to save your score of " + timer;
 
     subBtn.addEventListener("click", function(event) {
-        event.preventDefault()
-        var userName = document.querySelector("#userName").value
+        event.preventDefault();
+        var userName = document.querySelector("#userName").value;
         if (userName == "") {
-            alert("Please enter your name in the input field")
+            alert("Please enter your initials in the input field");
         }else {
             localStorage.setItem("userScore", userName + ": " + (timer + 1))
             location.reload()
         }
-    })
+    });
 }
 
 function renderScore () {
     if (localStorage.getItem("userScore") !== null) {
-        showScore.textContent = localStorage.getItem("userScore")
+        showScore.textContent = localStorage.getItem("userScore");
     } else {
         showScore.textContent = "0"
     }
@@ -205,5 +205,5 @@ function clear() {
     localStorage.clear()
 }
 
-startbtn.addEventListener("click", begin)
-clearScore.addEventListener("click", clear)
+startbtn.addEventListener("click", begin);
+clearScore.addEventListener("click", clear);
